ea0bd588374479c02fa4b5aebd68d80bf17a9b43
Artur Gubaidullin <lapteuh@gmail.com>
2025-09-25 15:35:44 +0000

    Add struct

--- todos/src/lib.rs
+++ todos/src/lib.rs
@@ -1,8 +1,10 @@
 mod error;
 mod new_todo;
 mod persisted_todo;
+mod service;
 mod todo;
 
 pub use error::Error;
 pub use new_todo::NewTodo;
+pub use service::Service;
 pub use todo::Todo;
---
+++ todos/src/service.rs
@@ -0,0 +1,90 @@
+use crate::{error::Error, new_todo::NewTodo, persisted_todo::PersistedTodo, todo::Todo};
+use std::{
+    collections::HashMap,
+    sync::{Arc, RwLock},
+};
+
+pub struct Service {
+    db: Arc<RwLock<HashMap<Box<str>, PersistedTodo>>>,
+}
+
+impl Service {
+    #[must_use]
+    pub fn new() -> Self {
+        Self { db: Arc::default() }
+    }
+
+    pub fn add(&self, new_todo: &NewTodo) -> Result<Todo, Box<dyn std::error::Error>> {
+        let todo_id = "id";
+
+        let persisted_todo = PersistedTodo::new(new_todo.text());
+
+        let todo = Todo::new(new_todo.text());
+
+        self.db
+            .write()
+            .map_err(|_| Error::Internal)
+            .and_then(|mut db| {
+                if db.contains_key(todo_id) {
+                    return Err(Error::AlreadyExists);
+                }
+                db.insert(todo_id.into(), persisted_todo);
+
+                Ok(())
+            })?;
+
+        Ok(todo)
+    }
+
+    pub fn all(&self) -> Result<Vec<Todo>, Box<dyn std::error::Error>> {
+        let a = self
+            .db
+            .read()
+            .map_err(|_| Error::Internal)?
+            .values()
+            .map(Todo::from)
+            .collect();
+
+        Ok(a)
+    }
+}
+
+impl Default for Service {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create() {
+        let todos = Service::new();
+
+        assert_eq!(todos.all().unwrap().len(), 0);
+    }
+
+    #[test]
+    fn add() {
+        let new_todo = &NewTodo::new("Add new todo");
+
+        let todos = Service::new();
+
+        todos.add(new_todo).unwrap();
+
+        assert_eq!(todos.all().unwrap().len(), 1);
+    }
+
+    #[test]
+    fn add_twice() {
+        let new_todo = &NewTodo::new("Add new todo");
+
+        let todos = Service::new();
+
+        todos.add(new_todo).unwrap();
+
+        assert!(todos.add(new_todo).is_err());
+    }
+}
