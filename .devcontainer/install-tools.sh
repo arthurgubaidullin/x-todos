36874804698b3ed00424a8992fae8d96a4a158b0
Artur Gubaidullin <lapteuh@gmail.com>
2025-09-27 15:52:48 +0000

    Update install tools script

--- .devcontainer/install-tools.sh
+++ .devcontainer/install-tools.sh
@@ -3,9 +3,4 @@ set -x
 sudo apt-get update -y
 sudo apt install libclang-dev -y
 
-cargo install wasm-pack
-cargo install wasm-bindgen-cli
-
-export PATH="$HOME/.cargo/bin:$PATH"
-
 npm install turbo --global
