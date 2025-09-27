94279b5045190ca7ae959fb4857052860ac36969
Artur Gubaidullin <lapteuh@gmail.com>
2025-09-27 17:59:10 +0000

    Update struct

--- server-side/todos-axum/src/todo_list_resource.rs
+++ server-side/todos-axum/src/todo_list_resource.rs
@@ -3,11 +3,17 @@ use axum::{Json, response::IntoResponse};
 use serde::Serialize;
 
 #[derive(Debug, Serialize)]
-pub struct TodoList {
+pub struct TodoListResource {
     pub(crate) items: Vec<TodoResource>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) previous: Option<Box<str>>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) next: Option<Box<str>>,
 }
 
-impl IntoResponse for TodoList {
+impl IntoResponse for TodoListResource {
     fn into_response(self) -> axum::response::Response {
         Json(self).into_response()
     }
