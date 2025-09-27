ec09b8fdb9055104b0b0c69fe611b8355f05b623
Artur Gubaidullin <lapteuh@gmail.com>
2025-09-27 15:47:35 +0000

    Remove wasm package & reset web app

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,7 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), wasm(), topLevelAwait()],
+  plugins: [vue()],
 });
