826c3700a790231c2e0422c2d41e680c8c6c2b1c
Artur Gubaidullin <lapteuh@gmail.com>
2025-10-06 16:17:57 +0000

    Fix interface

--- client-side/web-client/fetch/src/client.ts
+++ client-side/web-client/fetch/src/client.ts
@@ -1,4 +1,4 @@
-import type { NewTodo, Todos, WebClient } from "@x-todos/web-client";
+import type { NewTodo, Todo, Todos, WebClient } from "@x-todos/web-client";
 import type { RootResource } from "./root-resource.js";
 import type { TodoListResource } from "./todo-list-resource.js";
 
@@ -25,17 +25,20 @@ export class FetchClient implements WebClient {
     return data;
   }
 
-  async add(this: this, newTodo: NewTodo): Promise<Todos> {
+  async add(this: this, newTodo: NewTodo): Promise<Todo> {
     const relations = await this.relations();
 
-    await fetch(relations.create.replace(":id", crypto.randomUUID()), {
-      method: "PUT",
-      body: JSON.stringify(newTodo),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const response = await fetch(
+      relations.create.replace(":id", crypto.randomUUID()),
+      {
+        method: "PUT",
+        body: JSON.stringify(newTodo),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-    return await this.todos();
+    return await response.json();
   }
 }
--- client-side/web-client/interface/src/web-client.ts
+++ client-side/web-client/interface/src/web-client.ts
@@ -1,7 +1,8 @@
 import type { NewTodo } from "./new-todo.js";
 import type { Todos } from "./todo-list.js";
+import type { Todo } from "./todo.js";
 
 export interface WebClient {
   readonly todos: () => Promise<Todos>;
-  readonly add: (newTodo: NewTodo) => Promise<Todos>;
+  readonly add: (newTodo: NewTodo) => Promise<Todo>;
 }
