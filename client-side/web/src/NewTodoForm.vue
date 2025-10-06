99be4133302f4ef4a5cc5dd61339e520059c296b
Artur Gubaidullin <lapteuh@gmail.com>
2025-10-06 14:28:33 +0000

    Add vue component

---
+++ client-side/web/src/NewTodoForm.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const onSubmit = (event: Event) => {
+  event.preventDefault();
+
+  alert("Ok!");
+};
+</script>
+
+<template>
+  <form v-on:submit="onSubmit">
+    <h1>New Todo</h1>
+
+    <label>
+      Text:
+      <input type="text" name="text" required />
+    </label>
+
+    <input type="submit" />
+  </form>
+</template>
