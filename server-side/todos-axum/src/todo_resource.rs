fec49d3c28fce42ea765357c5ed629f6d8826e58
Artur Gubaidullin <lapteuh@gmail.com>
2025-10-06 05:22:10 +0000

    Implement trait

--- server-side/todos-axum/src/todo_resource.rs
+++ server-side/todos-axum/src/todo_resource.rs
@@ -7,6 +7,15 @@ pub struct TodoResource {
     pub(crate) text: Box<str>,
 }
 
+impl From<todos::Todo> for TodoResource {
+    fn from(value: todos::Todo) -> Self {
+        Self {
+            id: value.id().into(),
+            text: value.text().into(),
+        }
+    }
+}
+
 impl IntoResponse for TodoResource {
     fn into_response(self) -> axum::response::Response {
         Json(self).into_response()
