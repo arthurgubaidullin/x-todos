c688fb566850b9459c30cdf21eb435c5094c9f76
Artur Gubaidullin <lapteuh@gmail.com>
2025-10-07 10:33:18 +0000

    Remove interface

--- client-side/vue-client/interface/src/index.ts
+++ client-side/vue-client/interface/src/index.ts
@@ -2,4 +2,3 @@ export * from "./add-new-todo.js";
 export * from "./get-todos.js";
 export * from "./new-todo.js";
 export * from "./todo.js";
-export * from "./vue-client.js";
--- client-side/vue-client/interface/src/vue-client.ts
+++
@@ -1,4 +0,0 @@
-import type { AddNewTodo } from "./add-new-todo.js";
-import type { GetTodos } from "./get-todos.js";
-
-export interface VueClient extends GetTodos, AddNewTodo {}
